6a3df7a3473464bcb291959feb7f96ce9233e9d1
oceanicbloom0 <shenlebanshashi@gmail.com>
2025-05-05 19:26:19 +0800

    fix err in script

--- scripts/openwrt/rename-bins.sh
+++ scripts/openwrt/rename-bins.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 DATE=$(date +[%Y-%m-%d])
-REPO=$(echo "$SOURCE_URL" | sed -E 's#https://github.com/([^/]+/[^/.]+)(\.git)?$#\1#')
 
-# 重命名
+REPO_SANITIZED=$(echo "$SOURCE_URL" \
+  | sed -E 's#https://github.com/([^/]+/[^/.]+)(\.git)?$#\1#' \
+  | tr '/' '_')
+
+# 执行重命名循环
 for file in openwrt*; do
-    newname="[${REPO}] ${DATE} ${FIRMWARE_NAME}${file#openwrt}"
-    mv -- "$file" "$newname"
+  mv -- "$file" \
+    "[${REPO_SANITIZED}] ${DATE} ${FIRMWARE_NAME}${file#openwrt}"
 done
+
+# 查看最终结果
+ls
