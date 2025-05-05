a21734ecbb911403945c59a30debbdd81a61d7d9
oceanicbloom0 <shenlebanshashi@gmail.com>
2025-05-05 18:49:24 +0800

    +

--- scripts/openwrt/rename-bins.sh
+++ scripts/openwrt/rename-bins.sh
@@ -3,7 +3,8 @@
 DATE=$(date +[%Y-%m-%d])
 REPO=$(echo "$SOURCE_URL" | sed -E 's#https://github.com/([^/]+/[^/.]+)(\.git)?$#\1#')
 
+# 重命名
 for file in openwrt*; do
-    newname=$(echo "$file" | sed "s#^openwrt#[${REPO}] ${DATE} ${FIRMWARE_NAME}#")
-    mv "$file" "$newname"
+    newname="[${REPO}] ${DATE} ${FIRMWARE_NAME}${file#openwrt}"
+    mv -- "$file" "$newname"
 done
