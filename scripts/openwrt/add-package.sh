f75cfe681a157e32affab6353802917c60840dfe
oceanicbloom0 <shenlebanshashi@gmail.com>
2025-05-17 10:49:38 +0800

    修复 nikki 依赖缺失

--- scripts/openwrt/add-package.sh
+++ scripts/openwrt/add-package.sh
@@ -5,7 +5,7 @@ cd package
 ## Passwall Tools
 git clone --depth 1 https://github.com/vernesong/OpenClash && mv -n OpenClash/luci-app-openclash ./
 rm -rf OpenClash
-git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki && mv -n OpenWrt-nikki/luci-app-nikki ./
+git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki && mv -n OpenWrt-nikki/luci-app-nikki OpenWrt-nikki/nikki ./
 rm -rf OpenWrt-nikki
 
 ## Amlogic
