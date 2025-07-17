ee1ea9e760f3675b3a7a432349b719fcc1b52aa2
oceanicbloom0 <shenlebanshashi@gmail.com>
2025-07-17 14:48:50 +0800

    Refactor app configuration appending logic in copy-config.sh

--- scripts/openwrt/copy-config.sh
+++ scripts/openwrt/copy-config.sh
@@ -5,7 +5,10 @@ STANDALONE_CONF_PATH="$GITHUB_WORKSPACE/configs/STANDALONE_CONF/$DEVICE/.config"
 
 if [ -f "$CONFIG_PATH" ]; then
     cp "$CONFIG_PATH" "$OPENWRT_PATH/.config"
-    cat "$GITHUB_WORKSPACE/$APP_CONFIG" >> "$OPENWRT_PATH/.config"
+    if [ -n "$APP_CONFIG" ] && [ -f "$GITHUB_WORKSPACE/$APP_CONFIG" ]; then
+        # Append additional app configuration if specified
+        cat "$GITHUB_WORKSPACE/$APP_CONFIG" >>"$OPENWRT_PATH/.config"
+    fi
 elif [ -f "$STANDALONE_CONF_PATH" ]; then
     cp "$STANDALONE_CONF_PATH" "$OPENWRT_PATH/.config"
 else
