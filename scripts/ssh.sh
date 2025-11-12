250909c339b1fcb424a58a36604db27d07a9aed7
oceanicbloom0 <shenlebanshashi@gmail.com>
2025-11-12 23:39:04 +0800

    +

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -4,29 +4,23 @@ set -uo pipefail
 
 OWNER="$1"
 
-curl -fsSLO https://starship.rs/install.sh && ARCH= sh ./install.sh --yes
-rm ./install.sh
-echo 'eval "$(starship init bash)"' >>/home/runner/.bashrc
-echo 'eval "$(starship init bash)"' | sudo tee /root/.bashrc
-
 mkdir -p /home/runner/.ssh
 
 # Downloading Your Public Keys from the Repository
 curl -sL "https://github.com/${OWNER}.keys" -o ./github_keys
-
 if [ -s ./github_keys ]; then
-	cat ./github_keys >>/home/runner/.ssh/authorized_keys
-	chmod 600 /home/runner/.ssh/authorized_keys
+    cat ./github_keys >>/home/runner/.ssh/authorized_keys
+    chmod 600 /home/runner/.ssh/authorized_keys
 else
-	echo "❌ Failed to fetch SSH keys for $OWNER or keys are empty."
-	exit 1
+    echo "❌ Failed to fetch SSH keys for $OWNER or keys are empty."
+    exit 1
 fi
 
 # fetch external repository
-echo "$EXT_REPO_RSA" >~/.ssh/ext_repo_rsa
-chmod 600 ~/.ssh/ext_repo_rsa
+echo "$ACTION_RSA" >~/.ssh/ACTION_RSA
+chmod 600 ~/.ssh/ACTION_RSA
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/ext_repo_rsa
+ssh-add ~/.ssh/ACTION_RSA
 
 mkdir -p ~/ext-repo
 cd ~/ext-repo
@@ -34,8 +28,12 @@ git init
 git remote add origin git@github.com:${EXT_REPO}.git
 git fetch
 git checkout -t origin/main
-
 bash ./deploy.sh >/dev/null 2>&1
+
+curl -fsSLO https://starship.rs/install.sh && ARCH= sh ./install.sh --yes
+rm ./install.sh
+echo 'eval "$(starship init bash)"' >>/home/runner/.bashrc
+echo 'eval "$(starship init bash)"' | sudo tee /root/.bashrc
 
 # Block. Stop this container to continue
 docker run --name cloudflared --net=host cloudflare/cloudflared:latest tunnel --no-autoupdate run --token $CLOUDFLARED_TOKEN || true
