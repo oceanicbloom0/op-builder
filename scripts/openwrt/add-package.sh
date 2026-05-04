ef228f696ef3d4c6962baad843de58073caa2ca4
oceanicbloom0 <me@xhscdn.help>
2026-05-04 14:47:04 +0800

    feat: use generic function

--- scripts/openwrt/add-package.sh
+++ scripts/openwrt/add-package.sh
@@ -2,17 +2,32 @@
 
 cd package/feeds/luci
 
+git_clone_clean() {
+    local repo="$1"
+    local dir
+    
+    dir="$(basename "$repo" .git)"
+    
+    if ! git clone --depth 1 "$repo" "$dir"; then
+        echo "Clone failed. Removing existing directory: $dir"
+        rm -rf -- "$dir"
+        git clone --depth 1 "$repo" "$dir"
+    fi
+}
+
+
 ## Passwall Tools
-git clone --depth 1 https://github.com/vernesong/OpenClash && mv -n OpenClash/luci-app-openclash ./ && rm -rf OpenClash
-git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki && mv -n OpenWrt-nikki/luci-app-nikki OpenWrt-nikki/nikki ./ && rm -rf OpenWrt-nikki
+git_clone_clean  https://github.com/vernesong/OpenClash && mv -n OpenClash/luci-app-openclash ./ && rm -rf OpenClash
+git_clone_clean https://github.com/nikkinikki-org/OpenWrt-nikki && mv -n OpenWrt-nikki/luci-app-nikki OpenWrt-nikki/nikki ./ && rm -rf OpenWrt-nikki
 
 ## Amlogic
-git clone --depth 1 https://github.com/ophub/luci-app-amlogic.git op-amlogic && mv -n op-amlogic/luci-app-amlogic ./ && rm -rf op-amlogic
+git_clone_clean https://github.com/ophub/luci-app-amlogic.git op-amlogic && mv -n op-amlogic/luci-app-amlogic ./ && rm -rf op-amlogic
 
 ## Themes
-git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
-git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
-git clone --depth 1 https://github.com/AngelaCooljx/luci-theme-material3.git
+git_clone_clean https://github.com/jerrykuku/luci-theme-argon.git
+git_clone_clean https://github.com/jerrykuku/luci-app-argon-config.git
+git_clone_clean https://github.com/AngelaCooljx/luci-theme-material3.git
+
 # 更改 material3 主题 makefile
 sed -i 's#^include \.\./\.\./luci.mk#include $(TOPDIR)/feeds/luci/luci.mk#' luci-theme-material3/Makefile
 exit 0
